51bffc5463349bcbacc60a5ca33d510d0fc83cd7
plokmij <samfan@live.com>
2019-02-20 15:10:09 +0530

    Add profile_ui widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import '';
 
 void main() {
   runApp(App());
@@ -13,10 +14,15 @@ class App extends StatelessWidget {
           headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
             return <Widget>[
               SliverAppBar(
-                title: Text("Test"),
-                expandedHeight: 248.0,
-                floating: false,
+                backgroundColor: Colors.black,
+                title: Text("Profile"),
+                expandedHeight: 320.0,
                 pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  background: ProfileCard(fullName: 'Samfan',phoneNo: '+91-8129127294',),
+                ),
+                
                 /*flexibleSpace: FlexibleSpaceBar(
                   centerTitle: true,
                   title: Text("Collapsing Toolbar",
@@ -32,9 +38,138 @@ class App extends StatelessWidget {
               ),
             ];
           },
-          body: Text('Nothingaksd asdaskj'),
+          body: ListView(
+            children: <Widget>[
+              OptionList(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  final String fullName;
+  final String phoneNo;
+
+  ProfileCard({this.fullName, this.phoneNo});
+
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[card(context)],
+    );
+  }
+
+  Widget card(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top:80.0),
+      child: Stack(
+        children: <Widget>[
+          colorBox(Colors.black),
+          SizedBox(height:10.0),
+          profilePic(context),
+          nameNnumber(),
+        ],
+      ),
+    );
+  }
+
+  Widget colorBox(Color color) {
+    return Container(
+      height: 248.0,
+      decoration: BoxDecoration(
+          color: Colors.black,
+          shape: BoxShape.rectangle,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              blurRadius: 2.0,
+              offset: Offset(0.0, 10.0),
+            )
+          ]),
+    );
+  }
+
+  Widget profilePic(BuildContext context) {
+    //double width = MediaQuery.of(context).size.width / 2 - 64.0;
+    return Container(
+      alignment: Alignment.center,
+      child: Icon(
+        Icons.account_circle,
+        size: 124.0,
+        color: Colors.black,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+
+  Widget nameNnumber() {
+    Text name = Text(
+      fullName,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 21.0,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+
+    Text number = Text(
+      phoneNo,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 15.0,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.only(
+        top: 155.0,
+      ),
+      child: Column(
+        children: <Widget>[
+          name,
+          SizedBox(
+            height: 1.0,
+          ),
+          number
+        ],
+      ),
+    );
+  }
+}
+
+class OptionList extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        buildTile(context, "Contact Details", Icons.smartphone),
+        buildTile(context, "Booking History", Icons.history),
+        buildTile(context, "Send Feedback", Icons.feedback),
+        buildTile(context, "Invite", Icons.supervisor_account),
+        buildTile(context, "Notification Preferences", Icons.notification_important),
+        
+      ],
+    );
+  }
+
+  Widget buildTile(BuildContext context, String title, IconData icon) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(title),
+          leading: Icon(icon),
+          trailing: Icon(Icons.navigate_next),
+        ),
+        Divider(
+          height: 6.0,
+        ),
+      ],
     );
   }
 }
